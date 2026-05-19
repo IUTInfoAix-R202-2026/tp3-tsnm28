9481872e71a3035e8d63e2f0e2ac33883008620f
Tasnim <tasnimsebri28etude@gmail.com>
2026-05-19 12:07:32 +0000

    Exercice6 finis

--- src/main/java/fr/univ_amu/iut/exercice6/EnTeteController.java
+++ src/main/java/fr/univ_amu/iut/exercice6/EnTeteController.java
@@ -29,6 +29,7 @@ public class EnTeteController {
     // TODO exercice 6 : déclencher actionNouveauSite.run() à chaque clic sur boutonNouveauSite.
     // (Astuce : boutonNouveauSite.setOnAction(e -> ...). On évite onAction="#..." dans le FXML
     //  car la cible de l'action est fournie dynamiquement par le contrôleur parent.)
+    boutonNouveauSite.setOnAction(e -> actionNouveauSite.run());
     mettreAJourCompteur(0);
   }
 
@@ -50,5 +51,12 @@ public class EnTeteController {
     //   - "Aucun site déclaré"             si nombreSites == 0
     //   - "1 site déclaré"                  si nombreSites == 1
     //   - "<nombreSites> sites déclarés"   sinon
+    if (nombreSites == 0) {
+      labelSousTitre.setText("Aucun site déclaré");
+    } else if (nombreSites == 1) {
+      labelSousTitre.setText("1 site déclaré");
+    } else {
+      labelSousTitre.setText(nombreSites + " sites déclarés");
+    }
   }
 }
